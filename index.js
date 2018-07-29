1502c36cca292fcb4f389709adb8fb7b2c75a3b3
M. Alif Raher <alifraher@gmail.com>
2018-07-29 21:32:52 +0700

    Add Level 0

---
+++ index.js
@@ -0,0 +1,15 @@
+const names = [
+    "Mr. Fahri",
+    "Mr. Abdul",
+    "Mrs. Josephhine",
+    "Mr. Joseph",
+    "Mr. Paul",
+    "Mrs. Paula",
+    "Mr. Hakim"
+];
+
+console.log("Level 0");
+
+names.forEach(function (element) {
+    console.log(element);
+});
